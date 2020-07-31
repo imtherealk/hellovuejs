bd07c79ad99bc8ef04bee3e5a41108f9d4e5d684
Nayeong <imtherealk@gmail.com>
2020-07-31 15:51:22 +0900

    TodoList component와 삭제 기능 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>Todo List</div>
+  <div>
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" v-bind:key="index" class="shadow">
+        {{todoItem}}
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return {
+      todoItems: []
+    }
+  },
+  methods: {
+    removeTodo: function(item, index) {
+      localStorage.removeItem(item);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  created: function() {
+    if (localStorage.length > 0) {
+      for (let i=0; i < localStorage.length; i++){
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+  },
 }
 </script>
 
 <style>
-
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+  font-size: 24px
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
 </style>
